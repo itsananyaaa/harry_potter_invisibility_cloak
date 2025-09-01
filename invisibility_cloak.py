e5536d2587012a49cc64461e8c44355b59dfedf1
Ananya Biju <ananya.bijuks@gmail.com>
2025-09-01 14:45:39 +0400

    Add files via upload

---
+++ invisibility_cloak.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+time.sleep(3)       
+
+background = 0
+
+for i in range(60):
+    ret, background = cap.read()
+background = np.flip(background,axis=1)
+
+while(cap.isOpened()): 
+    ret, img = cap.read()
+    if not ret:
+        break
+    img = np.flip(img,axis=1)
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    
+    lower_yellow = np.array([20,120,70])
+    upper_yellow = np.array([35,255,255])
+    mask1 = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+    lower_yellow2 = np.array([35,120,70])
+    upper_yellow2 = np.array([40,255,255])
+    mask2 = cv2.inRange(hsv, lower_yellow2, upper_yellow2)
+
+    mask1 = mask1 + mask2
+
+    mask1 = cv2.morphologyEx(mask1,cv2.MORPH_OPEN,np.ones((3,3),np.uint8), iterations = 2)
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE,np.ones((3,3),np.uint8), iterations = 1)
+
+    mask2 =cv2.bitwise_not(mask1)
+
+    res1 = cv2.bitwise_and(background,background,mask=mask1)
+    res2 = cv2.bitwise_and(img,img,mask=mask2)
+    final_output = cv2.addWeighted(res1,1,res2,1,0)
+    cv2.imshow('Invisible Cloak',final_output)
+    k = cv2.waitKey(10)
+    if k==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
